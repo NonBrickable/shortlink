eb44f59d280773aab62ecb74da0143b718639b4e
lvcq <1594850919@qq.com>
2023-11-09 09:58:22 +0800

    用户登录有效时间修改

--- admin/src/main/java/com/lv/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/lv/shortlink/admin/service/impl/UserServiceImpl.java
@@ -103,7 +103,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         }
         String uuid = UUID.randomUUID().toString();
         stringRedisTemplate.opsForHash().put("login_" + requestParam.getUsername(),uuid, JSON.toJSONString(userDO));
-        stringRedisTemplate.expire("login_" + requestParam.getUsername(),30, TimeUnit.MINUTES);
+        stringRedisTemplate.expire("login_" + requestParam.getUsername(),30, TimeUnit.DAYS);
         return new UserLoginRespDTO(uuid);
     }
 
