751f0ee612af919c443dba119aa754be182339f6
lvcq <1594850919@qq.com>
2023-11-10 21:26:02 +0800

    初始化短链接项目基础组件

---
+++ project/src/main/java/com/lv/shortlink/project/common/convention/result/Result.java
@@ -0,0 +1,39 @@
+package com.lv.shortlink.project.common.convention.result;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@Accessors(chain = true)
+public class Result<T> implements Serializable {
+    /**
+     * 正确返回码
+     */
+    public static final String SUCCESS_CODE = "0";
+
+    /**
+     * 返回码
+     */
+    private String code;
+
+    /**
+     * 返回信息
+     */
+    private String message;
+
+    /**
+     * 响应数据
+     */
+    private T data;
+
+    /**
+     * 请求ID
+     */
+    private String requestId;
+
+    public boolean isSuccess() {
+        return SUCCESS_CODE.equals(code);
+    }
+}
---
+++ project/src/main/java/com/lv/shortlink/project/config/MyMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.lv.shortlink.project.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 自动填充配置
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", () -> new Date(), Date.class);
+        this.strictInsertFill(metaObject, "updateTime", () -> new Date(), Date.class);
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+        this.strictInsertFill(metaObject, "sortOrder", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", () -> new Date(), Date.class);
+    }
+}
