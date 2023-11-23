2ad1ba1a523fb9875bce74f1c9abaecca48f0148
lvcq <1594850919@qq.com>
2023-11-23 19:54:24 +0800

    refactor：方法重命名

--- project/src/main/java/com/lv/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
+++ project/src/main/java/com/lv/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -15,5 +15,6 @@ public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
     @Insert("INSERT INTO t_link_locale_stats (full_short_url, gid, date, cnt, country, province, city, adcode, create_time, update_time, del_flag) " +
             "VALUES( #{linkLocaleStats.fullShortUrl}, #{linkLocaleStats.gid}, #{linkLocaleStats.date},#{linkLocaleStats.cnt}, #{linkLocaleStats.country}, #{linkLocaleStats.province}, #{linkLocaleStats.city}, #{linkLocaleStats.adcode}, NOW(), NOW(), 0) " +
             "ON DUPLICATE KEY UPDATE cnt = cnt + #{linkLocaleStats.cnt};")
-    void shortLinkLocalStats(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
+    void shortLinkLocaleStats(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
 }
+
