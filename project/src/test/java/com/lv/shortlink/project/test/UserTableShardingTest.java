9cb9a8891555100bba175a59da528fba7cd71a97
lvcq <1594850919@qq.com>
2023-11-28 20:15:19 +0800

    feature: 开发短链接监控之今日访问分表规则

--- project/src/test/java/com/lv/shortlink/project/test/UserTableShardingTest.java
+++ project/src/test/java/com/lv/shortlink/project/test/UserTableShardingTest.java
@@ -2,19 +2,19 @@ package com.lv.shortlink.project.test;
 
 public class UserTableShardingTest {
     public static final String SQL = "CREATE TABLE `t_link_stats_today_%d` (\n" +
-            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
             "  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识',\n" +
             "  `full_short_url` varchar(128) DEFAULT NULL COMMENT '短链接',\n" +
             "  `date` date DEFAULT NULL COMMENT '日期',\n" +
-            "  `today_pv` int(11) DEFAULT '0' COMMENT '今日PV',\n" +
-            "  `today_uv` int(11) DEFAULT '0' COMMENT '今日UV',\n" +
-            "  `today_uip` int(11) DEFAULT '0' COMMENT '今日IP数',\n" +
+            "  `today_pv` int DEFAULT '0' COMMENT '今日PV',\n" +
+            "  `today_uv` int DEFAULT '0' COMMENT '今日UV',\n" +
+            "  `today_uip` int DEFAULT '0' COMMENT '今日IP数',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
             "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `idx_unique_full-short-url` (`full_short_url`) USING BTREE\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;;";
+            "  UNIQUE KEY `idx_unique_full-short-url` (`full_short_url`,`gid`,`date`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
 
 
     public static void main(String[] args) {
